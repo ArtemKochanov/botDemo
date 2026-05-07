44c038873740e7c05ad2bb82aaa01301cabcb482
ArtemKochanov <artem06092001@yandex.ru>
2026-05-07 18:15:07 +0300

    add moderation

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.types import FSInputFile
 from aiogram.client.default import DefaultBotProperties
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,15 +27,62 @@ RULES_TEXT = """📌 Добро пожаловать в чат
 Всем удачи...
 """
 
+BAD_WORDS = [
+    "хуй",
+    "залупа",
+    "пизда",
+    "гандон",
+]
+
+warnings = {}
+
+keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="💖 Поддержать автора",
+                url="https://www.tinkoff.ru/rm/r_qEkoNlpkfl.hcwycHOMII/1fRzk69171"
+            )
+        ]
+    ]
+)
+
 @dp.message()
 async def handle_message(message: types.Message):
+
+    # ПОСТЫ КАНАЛА
     if message.sender_chat and message.sender_chat.type == "channel":
+
         photo = FSInputFile("image.jpg")
 
         await message.reply_photo(
             photo=photo,
-            caption=RULES_TEXT
+            caption=RULES_TEXT,
+            reply_markup=keyboard
         )
+
+        return
+
+    # МОДЕРАЦИЯ
+    if not message.text:
+        return
+
+    text = message.text.lower()
+
+    for bad_word in BAD_WORDS:
+        if bad_word in text:
+
+            user_id = message.from_user.id
+
+            warnings[user_id] = warnings.get(user_id, 0) + 1
+
+            count = warnings[user_id]
+
+            await message.reply(
+                f"⚠️ {message.from_user.full_name}, предупреждение ({count}/3)"
+            )
+
+            break
 
 async def main():
     await dp.start_polling(bot)
